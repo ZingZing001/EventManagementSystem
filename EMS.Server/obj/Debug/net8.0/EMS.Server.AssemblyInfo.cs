00a44dbe78f3c094b9aecf51f38cda480e5dad1a
ZingZing_ <runjiazhang.nz@gmail.com>
2024-06-30 14:54:49 +1200

    chore: Update EMS.Server dependencies to Microsoft.EntityFrameworkCore 8.0.6

--- EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
+++ EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce79c4f69e29504cd8f1f10de9c3ab5f9e47a83c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71eac42ba47ae9406babf49ef07992b3d1df7930")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
