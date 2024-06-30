a3e924228dce453d12b7717177a903ab0ce9e5ab
ZingZing_ <runjiazhang.nz@gmail.com>
2024-06-30 15:00:50 +1200

    chore: Update EMS.Server.AssemblyInfo.cs version to 1.0.0+0be2b1f8724aa7d3cd60a90d371cf65a1e4d5dc3

--- EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
+++ EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca5cbbda63eff83f930eabfdcf5567efb671944d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0be2b1f8724aa7d3cd60a90d371cf65a1e4d5dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
