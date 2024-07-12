31bd54af6dad926c6d059c91474706cd5b4cd09a
ZingZing_ <runjiazhang.nz@gmail.com>
2024-07-12 15:21:59 +1200

    chore: Update database connection string and add ApplicationDbContext class

--- EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
+++ EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcd60b5426152419d59bb97bd1b48114c26a33d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b6ed597b8473369c904fe0b6fc71ff2a157e26")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
