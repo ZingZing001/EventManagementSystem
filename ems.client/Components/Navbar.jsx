9b8346374ccf10398c51b3a5e76400572be16428
ZingZing_ <runjiazhang.nz@gmail.com>
2024-07-16 15:00:44 +1200

    new: Added Navbar feature as part of the frontend

---
+++ ems.client/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          Event Management
+        </Typography>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/events">Events</Button>
+        <Button color="inherit" component={Link} to="/create-event">Create Event</Button>
+        <Button color="inherit" component={Link} to="/profile">Profile</Button>
+        <Button color="inherit" component={Link} to="/bookings">Bookings</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
