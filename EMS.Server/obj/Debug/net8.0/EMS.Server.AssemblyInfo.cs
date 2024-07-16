1b4b78cee38779d589074021b9789f28552b01b1
ZingZing_ <runjiazhang.nz@gmail.com>
2024-07-16 15:18:22 +1200

    feat: Update EMS.Server project configuration and paths

--- EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
+++ EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da5601bf970b96dc770cd5b769b350846a2bffea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b3252c06e9b00e78db7788c8bd8471c349332c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
