a6673ea896be8a1a2781ede16f3b3169c344d4c8
ZingZing_ <runjiazhang.nz@gmail.com>
2024-07-16 17:30:16 +1200

    refactor: Update EMS.Server project configuration and paths

--- EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
+++ EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a176fc565a1eba2916f6afd941fc95a8308a9741")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d5143cd639164627e985cb86501dde38563cbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
