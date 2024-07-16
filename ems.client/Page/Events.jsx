b0571772488bb889fb4efc905e5583c4e31b39c1
ZingZing_ <runjiazhang.nz@gmail.com>
2024-07-16 15:02:02 +1200

    new: Add Events component to display events with EventCard

---
+++ ems.client/Page/Events.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Grid, Typography } from '@mui/material';
+import EventCard from '../components/EventCard';
+import { fetchEvents } from '../services/api';
+
+const Events = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetchEvents().then(data => setEvents(data));
+  }, []);
+
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Events
+      </Typography>
+      <Grid container spacing={4}>
+        {events.map(event => (
+          <Grid item key={event.id} xs={12} sm={6} md={4}>
+            <EventCard event={event} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default Events;
