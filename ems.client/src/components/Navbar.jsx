de046af76b684ada515dbff260a80e1b0a375f98
ZingZing_ <runjiazhang.nz@gmail.com>
2024-07-16 16:50:56 +1200

    refactor: Add login and registration buttons to Navbar component

--- ems.client/src/components/Navbar.jsx
+++ ems.client/src/components/Navbar.jsx
@@ -14,6 +14,9 @@ const Navbar = () => {
         <Button color="inherit" component={Link} to="/create-event">Create Event</Button>
         <Button color="inherit" component={Link} to="/profile">Profile</Button>
         <Button color="inherit" component={Link} to="/bookings">Bookings</Button>
+        <Button color="inherit" component={Link} to="/login">Login</Button>
+        <Button color="inherit" component={Link} to="/register">Register</Button>
+
       </Toolbar>
     </AppBar>
   );
