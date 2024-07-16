b9b9ba2f61647419aafe8e3b50bf3bea8c808e9c
ZingZing_ <runjiazhang.nz@gmail.com>
2024-07-16 14:55:43 +1200

    chore: Update database connection string and add ApplicationDbContext class

--- EMS.Server/Controllers/BookingController.cs
+++ EMS.Server/Controllers/BookingController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using EventManagementAPI.Data;
-using EventManagementAPI.Models;
+
 
 [Route("api/[controller]")]
 [ApiController]
--- EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
+++ EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d97240ea2a930ff3d47a425503edd982a519263b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+355b7fd850ade66d209d82e7bf800f7c520d5ed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
