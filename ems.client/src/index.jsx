06d2d729bd8f1fb1cf95a22453142c2b4eceb49a
ZingZing_ <runjiazhang.nz@gmail.com>
2024-07-16 17:30:19 +1200

    refactor: Update EMS.Client project configuration and paths

--- ems.client/src/index.jsx
+++ ems.client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 
 ReactDOM.render(
