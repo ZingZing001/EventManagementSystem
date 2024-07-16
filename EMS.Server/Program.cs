be30a9df0b218645f0ae1136e854f4b604e99630
ZingZing_ <runjiazhang.nz@gmail.com>
2024-07-16 14:49:40 +1200

    chore: Update database connection string and add ApplicationDbContext class

--- EMS.Server/Program.cs
+++ EMS.Server/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext") ??throw new InvalidOperationException("Connection string 'StudentContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext") ??throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
+++ EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b6ed597b8473369c904fe0b6fc71ff2a157e26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31bd54af6dad926c6d059c91474706cd5b4cd09a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
