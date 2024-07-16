355b7fd850ade66d209d82e7bf800f7c520d5ed1
ZingZing_ <runjiazhang.nz@gmail.com>
2024-07-16 14:54:22 +1200

    new: added Booking controller to handle any booking sided infromation CRUD

--- EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
+++ EMS.Server/obj/Debug/net8.0/EMS.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+408a69cbc197fdbc7eb6af6ad101e887ca8c0e77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d97240ea2a930ff3d47a425503edd982a519263b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
